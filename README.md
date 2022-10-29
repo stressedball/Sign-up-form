1353bce5fd2ada7e1d7bbaa0bc10962837360cd4
stressedball <tserchesi@gmail.com>
2022-10-29 20:40:34 +0200

    Making 3 sizes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Sign-up-form
+
+Make a sign up form following the curriculem guidelines.
+
+Nothing fancy to add.
---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const introduction = document.getElementById('introduction');
+const banner = document.getElementById('banner'); 
+const forms = document.getElementById('form');
+const inputBoxes = document.querySelectorAll('input');
+document.addEventListener('keydown', checkInput);
+document.addEventListener('input', whichInput);
+window.addEventListener('load', first);
+
+function first(){
+    let introBottom = introduction.getBoundingClientRect().bottom;
+    banner.style.top = introBottom + 'px';
+}
+
+let inputTarget;
+
+function whichInput(e) {
+
+    inputTarget = document.querySelector(`input#${e.target.id} + span`);
+
+    if (e.target.id === 'firstName' || e.target.id === 'lastName') {
+        if (!e.target.value.match(/^[A-Za-z]+$/)) {
+            inputTarget.classList.remove('typeAgain')
+            inputTarget.classList.add('message');
+        } else {
+            inputTarget.classList.remove('message')
+            inputTarget.classList.add('typeAgain');
+        }
+        return;
+    }
+
+
+
+    if (e.target.id === 'email') {
+        if (!e.target.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+            inputTarget.style.visibility = 'visible';
+            return;
+        } else {
+            inputTarget.style.visibility = 'hidden';
+            return;
+        }
+    }
+
+
+}
+
+function checkInput(input) {
+
+}
+
